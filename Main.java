696f32d7c6f76cf00830e8f461a7e4a79052ab8a
takahiro-aoki494 <arikatuki@gmail.com>
2022-10-24 00:28:09 +0900

    Main User 修正

--- Main.java
+++ Main.java
@@ -1,20 +1,26 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> usersName = new ArrayList<>(Arrays.asList("佐藤", "鈴木", "田中"));
-        List<String> usersBirthplace = new ArrayList<>(Arrays.asList("東京", "青森", "大阪"));
-        List<String> usersBirthDay = new ArrayList<>(Arrays.asList("11月24日", "12月18日", "10月10日"));
+        //ユーザーのデータから各Userのインスタンスを生成
+        User satou = new User("佐藤", "11月24日", "東京");
+        User suzuki = new User("鈴木", "12月18日", "青森");
+        User tanaka = new User("田中", "10月10日", "大阪");
+
+        //各ユーザーのインスタンスを使いMapを生成
+        Map<String, String> userMap = new HashMap<>();
+        userMap.put(satou.getName(), "出身地は" + satou.getBirthPlace() + "。誕生日は" + satou.getBirthday());
+        userMap.put(suzuki.getName(), "出身地は" + suzuki.getBirthPlace() + "。誕生日は" + suzuki.getBirthday());
+        userMap.put(tanaka.getName(), "出身地は" + tanaka.getBirthPlace() + "。誕生日は" + tanaka.getBirthday());
+
+        //ユーザーのリストが入っているMapが格納されているユーザーチェック用インスタンスを生成
+        FindUser userLists = new FindUser(userMap);
 
         //入力用のスキャナーと入力結果取得用の変数
         Scanner sc = new Scanner(System.in);
         String result;
-
-        //ユーザーのリストが入っているMapが格納されているユーザー用インスタンスを生成
-        User userLists = new User(usersName, usersBirthplace, usersBirthDay);
 
         //標準入力からユーザー名を取得
         System.out.println("ユーザー名を入力");
--- User.java
+++ User.java
@@ -1,30 +1,28 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-
 public class User {
-    private final Map<String, String> userListMap = new HashMap<>();
+    private String name;
+    private String birthday;
+    private String birthPlace;
+
 
     //コンストラクタ
-    public User(List<String> name, List<String> birthplace, List<String> birthday) {
-        //引き渡されたListの大きさを取得
-        int cnt = name.size();
-
-        //引数を使いMapにユーザーの情報を格納する
-        for (int i = 0; i < cnt; i++) {
-            this.userListMap.put(name.get(i), "出身地は" + birthplace.get(i) + "。誕生日は" + birthday.get(i) + "です。");
-        }
+    public User(String name, String birthday, String birthPlace) {
+        this.name = name;
+        this.birthday = birthday;
+        this.birthPlace = birthPlace;
     }
 
-    //引数のユーザーでMapを検索し存在しないユーザーなら例外を発生
-    //存在するユーザーならユーザー名を返す
-    public String findUser(String userName) throws Exception {
-        String user = userListMap.get(userName);
 
-        if (Objects.equals(user, null)) {
-            throw new Exception("存在しないユーザーです");
-        }
-        return user;
+    //ゲッター
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
     }
 }
+
